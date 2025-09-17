39d1f9f015cd13caa8672bd7a25828b1f84243d5
Cristoffer Bohorquez <cristoffersmacbook@Cristoffers-MacBook-Air.local>
2025-09-17 10:30:34 -0400

    some other examples for the practice

---
+++ bunchConstants/main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int moonLanding = 1969;
+float lightSpeed = 299792458;
+float pi = 3.142;
+int hexaDead = 0xDEAD;
+int hexaSecret = 51966;
+
+
+
+int main(){
+
+    printf("            %10.d\n        ", moonLanding);
+
+    printf("00000000000 %10.d 000000000\n", moonLanding);
+
+    printf("%.0f\n", lightSpeed);
+
+    printf("%.1ef\n", lightSpeed);
+
+    printf("%.2f\n", pi);
+
+    printf("%.1ef\n", pi);
+
+    printf("%d\n", hexaDead);
+
+    printf("%d\n", hexaSecret);
+
+
+
+
+
+
+
+
+}
