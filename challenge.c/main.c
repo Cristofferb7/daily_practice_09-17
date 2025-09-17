15e5cad1b8f0dc0608856bd100c8c9147b72a4ee
Cristoffer Bohorquez <cristoffersmacbook@Cristoffers-MacBook-Air.local>
2025-09-17 09:49:43 -0400

    little challenge

---
+++ challenge.c/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+//space ship thrust 
+
+typedef enum {
+    none = 0,
+    low = 5,
+    medium = 9,
+    high = 12,
+    maximum = 20,
+
+
+
+} Thrust;
+
+int main (){
+
+printf("ready to go %d\n", none);
+printf("take off  %d\n", maximum);
+printf("entering the space %d\n", medium);
+printf("ready in deep space %d\n", low);
+
+}
